1b228e66f79002431fb68d6ce339198f23433ee3
M.C. van den Boogaart <scola@kvdb.name>
2016-10-20 19:02:52 +0200

    Add i18n

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,5 +1,10 @@
 export default {
   scola: {
+    auth: {
+      username: 'Username',
+      password: 'Password',
+      persistent: 'Keep me signed in'
+    },
     error: {
       invalid_password: 'Your username and/or password are incorrect.',
       invalid_token: 'An error occurred while logging in.'
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -1,5 +1,10 @@
 export default {
   scola: {
+    auth: {
+      username: 'Gebruikersnaam',
+      password: 'Wachtwoord',
+      persistent: 'Laat mij ingelogd blijven'
+    },
     error: {
       invalid_password: 'Uw gebruikersnaam en/of wachtwoord zijn niet juist.',
       invalid_token: 'Er is een fout opgetreden bij het inloggen.'
