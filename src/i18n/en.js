36eb3c6b5d49d75d62a6b1edfa24d1e1963c4af7
M.C. van den Boogaart <scola@kvdb.name>
2017-03-02 10:44:00 +0100

    Update i18n

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,14 +1,18 @@
 export default {
   scola: {
     auth: {
-      ok: 'OK',
       password: 'Password',
-      persistent: 'Keep me signed in',
+      persistent: 'Keep me logged in',
+      pop: {
+        ok: 'OK',
+        title: 'Login failed'
+      },
       username: 'Username'
     },
     error: {
       invalid_auth: 'You have no access.',
       invalid_credentials: 'Your username and/or password are incorrect.',
+      invalid_state: 'Your request is being processed.',
       invalid_token: 'Your session has expired.',
       invalid_user: 'You are not logged in.'
     }
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -1,14 +1,18 @@
 export default {
   scola: {
     auth: {
-      ok: 'OK',
       password: 'Wachtwoord',
       persistent: 'Laat mij ingelogd blijven',
+      pop: {
+        ok: 'OK',
+        title: 'Inloggen mislukt'
+      },
       username: 'Gebruikersnaam'
     },
     error: {
       invalid_auth: 'U hebt geen toegang.',
       invalid_credentials: 'Uw gebruikersnaam en/of wachtwoord zijn niet juist.',
+      invalid_state: 'Uw aanvraag wordt verwerkt.',
       invalid_token: 'Uw sessie is verlopen.',
       invalid_user: 'U bent niet ingelogd.'
     }
