088d21de53e98c27669d86fbe8c9474871390834
M.C. van den Boogaart <scola@kvdb.name>
2017-03-18 17:23:35 +0100

    Remove error message

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -12,7 +12,6 @@ export default {
     error: {
       invalid_auth: 'You have no access.',
       invalid_credentials: 'Your username and/or password are incorrect.',
-      invalid_state: 'Your request is being processed.',
       invalid_token: 'Your session has expired.',
       invalid_user: 'You are not logged in.'
     }
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -12,7 +12,6 @@ export default {
     error: {
       invalid_auth: 'U hebt geen toegang.',
       invalid_credentials: 'Uw gebruikersnaam en/of wachtwoord zijn niet juist.',
-      invalid_state: 'Uw aanvraag wordt verwerkt.',
       invalid_token: 'Uw sessie is verlopen.',
       invalid_user: 'U bent niet ingelogd.'
     }
